088cfe34a4f031be4fbe744e8e8f3d555414f665
dlj <anheihb03dlj@163.com>
2015-06-24 10:17:51 +0800

    上传钻孔设计对话框代码

---
+++ ARX_RCU/RcuEditOpenPoreDlg.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "RcuAcUiBaseDlg.h"
+#include "RcuDataLink.h"
+
+
+class RcuEditOpenPoreDlg : public RcuAcUiBaseDlg
+{
+	DECLARE_DYNAMIC(RcuEditOpenPoreDlg)
+
+public:
+	RcuEditOpenPoreDlg(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~RcuEditOpenPoreDlg();
+
+// 对话框数据
+	enum { IDD = IDD_RCU_OPEN_PORES_DLG };
+
+public:
+	//外部设置标题
+	CString m_title;
+	//外部设置钻场位置文本
+	CString m_pos;
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_num;
+	double m_gap;
+	double m_radius;
+
+	afx_msg void OnBnClickedOk();
+	virtual BOOL OnInitDialog();
+};
